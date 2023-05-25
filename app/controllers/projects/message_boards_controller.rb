a72771e059e644b73bf1977e51a886d0dda3872d
Trang T. Nguyen <trang@stackup.dev>
2023-05-25 13:23:09 +0700

    Fix typo error

---
+++ app/controllers/projects/message_boards_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Projects
+  class MessageBoardsController < ApplicationController
+    def index
+
+    end
+  end
+end
--- app/controllers/projects/message_borads_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Projects::MessageBoardsController < ApplicationController
-  def index
-
-  end
-end
