7d0a28ca6570487c29a98b562392c66c1a5bd680
Trang T. Nguyen <trang@stackup.dev>
2023-05-25 13:46:59 +0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+account = Account.create!(name: 'tester@test.co')
+proj = Project.create!(title: 'test project', account:)
