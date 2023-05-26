64d5c13f7499f2ff24c8e6cfbe29d808de0bc46d
Trang T. Nguyen <trang@stackup.dev>
2023-05-26 18:23:20 +0700

    Add create new project actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,24 @@ class ProjectsController < ApplicationController
   def show
     @project = Current.account.projects.find params[:id]
   end
+
+  def new
+    @project = Current.account.projects.new
+  end
+
+  def create
+    @project = Current.account.projects.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project), notice: "Project created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
