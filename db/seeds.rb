03b18a952ee0cf23a72b76a46ad3aa0a8a062753
Trang T. Nguyen <trang@stackup.dev>
2023-05-26 18:22:20 +0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-account = Account.create!(name: 'tester@test.co')
-proj = Project.create!(title: 'test project', account:)
+account = Account.create!(name: "tester@test.co")
+project = Project.create!(title: "test project", account:)
+bucket = Bucket.create!(account:, bucketable: project)
+
+2.times do |i|
+  message = Message.create!(subject: "message #{i}", content: "hello world")
+  recording = Recording.create!(bucket:, recordable: message)
+
+  2.times do |j|
+    comment = Comment.create!(content: "message #{i} comment #{i}#{j}")
+    Recording.create!(bucket:, recordable: comment, parent_id: recording.id)
+  end
+end
+
+message = Message.create!(subject: "message 3", content: "Mixing")
+recording = Recording.create!(bucket:, recordable: message)
+Comment.find_each do |comment|
+  Recording.create!(bucket:, recordable: comment, parent_id: recording.id)
+end
