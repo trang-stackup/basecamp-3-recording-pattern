2d821256561477538bb82adc1e3a9382f4916236
Trang T. Nguyen <trang@stackup.dev>
2023-05-29 06:53:15 +0700

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-account = Account.create!(name: "tester@test.co")
-project = Project.create!(title: "test project", account:)
-bucket = Bucket.create!(account:, bucketable: project)
+account_test = Account.create!(name: "test@abc.co")
+project = Project.create!(title: "abc project", account: account_test)
+bucket = Bucket.create!(account: account_test, bucketable: project)
 
 2.times do |i|
-  message = Message.create!(subject: "message #{i}", content: "hello world")
+  message = Message.create!(subject: "message #{i + 1}", content: "hello world #{i + 1}")
   recording = Recording.create!(bucket:, recordable: message)
 
   2.times do |j|
-    comment = Comment.create!(content: "message #{i} comment #{i}#{j}")
+    comment = Comment.create!(content: "message #{i + 1} comment #{i * 1 + j}")
     Recording.create!(bucket:, recordable: comment, parent_id: recording.id)
   end
 end
 
-message = Message.create!(subject: "message 3", content: "Mixing")
-recording = Recording.create!(bucket:, recordable: message)
-Comment.find_each do |comment|
-  Recording.create!(bucket:, recordable: comment, parent_id: recording.id)
-end
+message3 = Message.create!(subject: "message 3", content: "message 3 - Mixing comments of other messages")
+recording_msg3 = Recording.create!(bucket:, recordable: message3)
+
+# message 3 shares the same first comment with message 1
+message1 = Message.first
+comment1 = message1.recordings.sole.children.first.comment
+Recording.create!(bucket:, recordable: comment1, parent_id: recording_msg3.id)
+
+# message 3 shares the same third comment with message 1
+message2 = Message.second
+comment3 = message2.recordings.sole.children.first.comment
+Recording.create!(bucket:, recordable: comment3, parent_id: recording_msg3.id)
+
+# account_dev has the same project as the
+account_dev = Account.create!(name: "dev@abc.co")
+bucket_dev = Bucket.create!(account: account_dev, bucketable: project)
+
+message4 = Message.create!(subject: "message 4", content: "message 4 - hi there!")
+recording_msg4 = Recording.create!(bucket: bucket_dev, recordable: message4)
+
+message2 = Message.second
+comment2 = message2.recordings.sole.children.second
+Recording.create!(bucket: bucket_dev, recordable: comment2, parent_id: recording_msg4.id)
