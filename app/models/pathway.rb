f9bc0065e05bd42e95b51d51957aa869ee28ea1c
Trang T. Nguyen <trang@stackup.dev>
2023-06-02 18:23:36 +0700

    Add pathway parents association

--- app/models/pathway.rb
+++ app/models/pathway.rb
@@ -1,5 +1,6 @@
 class Pathway < ApplicationRecord
   include Recordable
 
+  has_many :communities, through: :parents, source: :recordable, source_type: "Community"
   has_many :skills, through: :children, source: :recordable, source_type: "Skill"
 end
