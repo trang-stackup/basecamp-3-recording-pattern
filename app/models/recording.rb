f3658028a5240f20a6b7b9082fc01a2d67d72c82
Trang T. Nguyen <trang@stackup.dev>
2023-06-09 14:04:25 +0700

    Update recordings to reference to account

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,7 +1,5 @@
 class Recording < ApplicationRecord
-  belongs_to :bucket
-  belongs_to :parent, class_name: "Recording", optional: true
-  has_many :children, class_name: "Recording", foreign_key: :parent_id
+  belongs_to :account
 
   delegated_type :recordable, types: %w[Pathway Skill LearningModule Tutorial]
 end
--- db/migrate/20200627104123_create_recordings.rb
+++ db/migrate/20200627104123_create_recordings.rb
@@ -1,6 +1,7 @@
 class CreateRecordings < ActiveRecord::Migration[6.1]
   def change
     create_table :recordings do |t|
+      t.references :account, null: false, foreign_key: true
       t.references :recordable, polymorphic: true, null: false
 
       t.timestamps
