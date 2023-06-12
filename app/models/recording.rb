7000d7b8375c85a08131ee80c992da836b89ddd5
Trang T. Nguyen <trang@stackup.dev>
2023-06-12 11:42:18 +0700

    Group similar associations

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -2,8 +2,9 @@ class Recording < ApplicationRecord
   belongs_to :account
 
   has_many :source_edges, class_name: "Edge", foreign_key: "source_id", dependent: :destroy
-  has_many :dest_edges, class_name: "Edge", foreign_key: "dest_id", dependent: :destroy
   has_many :edges, through: :source_edges, source: :dest
+
+  has_many :dest_edges, class_name: "Edge", foreign_key: "dest_id", dependent: :destroy
   has_many :inverse_edges, through: :dest_edges, source: :source
 
   delegated_type :recordable, types: %w[Community Pathway Skill LearningModule Tutorial]
